5e9b4eadf220a9657e1be717afd120e2cad9d464
amolagarwalece <amolagarwal@rocketmal.com>
2021-08-26 01:44:19 +0530

    updated the csv upload functionality

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const {models, sequelize} = require('./models/initSequelize')
 app.get('/', (req, res) => {
         res.send(`<h1>Welcome to Students Node.js utlity.</h1><br><a href='/upload'>upload from csv</a>
         <br><a href='/all'>Get all students</a>
-        <br><a href='/students'>Get Failed/Passed students</a>`)
+        <br><a href='/students?resultStatus=true'>Get Passed students</a><br>
+        <a href='/students?resultStatus=false'>Get Failed students</a>`)
   })
 
   app.get('/all', (req, res) => {
@@ -40,57 +41,81 @@ app.get('/', (req, res) => {
                 console.log('file uploaded successfully.')
                   //res.send('File uploaded')
               }
-          })
-          
-          
-        let path = (__dirname + '/uploads/'+ filename)
-          let stream = fs.createReadStream(path);
-          let csvData = [];
-          let csvStream = fastcsv
-            .parse()
-            .on("data", function(data) {
-              csvData.push(data);
-            })
-            .on("end", function() {
-              // remove the first line: header
-              csvData.shift();            
-             try{
-                for(let a of csvData){
+          }) 
+                let path = (__dirname + '/uploads/'+ filename)
+                let stream = fs.createReadStream(path);
+                let csvData = [];
+                let csvStream = fastcsv
+                .parse()
+            
+                .on("data", function(data) 
+                {
+                  csvData.push(data);
+                })
+            
+                .on("end", function() 
+                {
+                  csvData.shift();      // remove the first line: header      
+                  let fileExt = filename.split('.').pop();
+                  if(fileExt.toLowerCase() === 'csv'){
+                    insert(csvData)
+                    .then((ob)=>{
+                        res.send(`Inserted ${ob} rows successfully.`)
+                                })
+                  .catch((e)=>{
+                        res.send('Could not insert data', e)
+                              })
+                  }
+                  else{
+                    res.send('Cannot accept only .csv files!')
+                  }
+                  
+                  
+              })
+
+
+          const insert = async (data)=>{
+            try
+            { 
+              for(let a of data)
+                {
                   const query =`INSERT INTO students (id, name, age, mark1, mark2, mark3) 
                   VALUES (${a[0]},'${a[1]}',${a[2]},${a[3]},${a[4]},${a[5]})`
-                  sequelize.query(query)
-                  .then((result)=>{
-                    res.send('Inserting the data')
-                    count++
-                   // console.log('inserted rows: ', result[1]);
-                  })
-                  .catch((err)=>{
-                    console.log('Could not insert data', err)
-                  })
                   
-                 }
-                 //res.send('successful')
-              }
-              catch(error){
-                console.log(error)
-              }
-              })
+                  const [results, metadata] = await sequelize.query(query)
+                  
+                  count= count + metadata
+                       
+                  if(count == data.length)
+                          {
+                            return count
+                          }
+                
+                }
+            }
+            catch(error)
+            {
+              console.log(error)
+            }
+          }
           
           stream.pipe(csvStream);
 
       }
   })
 
-  app.get('/students/:id',(req,res)=>{
-    const getResult = async()=>{
-      let id= req.params.id;
-      let query = `select mark1, mark2, mark3 from students where id=${id}`
-      let marksObj = await sequelize.query(query)
-      let mark = marksObj[0]
-      let mark1 =  mark[0].mark1
-      let mark2 =  mark[0].mark2 
-      let mark3 =  mark[0].mark3
-      res.send(`<style>
+  app.get('/students/:id',(req,res)=>
+  {
+      const getResult = async()=>
+      {
+          let id= req.params.id;
+          let query = `select mark1, mark2, mark3 from students where id=${id}`
+          let marksObj = await sequelize.query(query)
+          let mark = marksObj[0]
+          let mark1 =  mark[0].mark1
+          let mark2 =  mark[0].mark2 
+          let mark3 =  mark[0].mark3
+            res.send(`<style>
       .demo {
         padding:5px;
       }
@@ -118,35 +143,44 @@ app.get('/', (req, res) => {
       </tr>
       
       </tbody>
-    </table>`)
+              </table>`)
      
-    }
-    getResult().then((o)=>{
+      }
+    getResult().then((o)=>
+      {
         console.log('done')
       })
-      .catch((e)=>{
+      .catch((e)=>
+      {
         console.log('error', e)
       })
   })
 
-  app.get('/students',(req,res)=>{
-    const getR = async()=>{
-      let st = req.query.resultStatus
-      let query = ''
-      if(st == true){
-       query = `select id, name from students where mark1 >40 and mark2 >40 and mark3 >40`
-      }
-      else{
-       query = `select * from students where mark1 <40 or mark2 <40 or mark3 <40`}
-      let marksObj = await sequelize.query(query)
-           res.send((marksObj[0]))
-    }
-    getR().then((o)=>{
-        console.log('done')
-      })
-      .catch((e)=>{
-        console.log('error', e)
-      })
+  app.get('/students',(req,res)=>
+  {
+      const getR = async()=>
+      {
+        let st = req.query.resultStatus
+        let query = ''
+        if(st == true)
+          {
+            query = `select id, name from students where mark1 >40 and mark2 >40 and mark3 >40`
+          }
+        else
+        {
+            query = `select * from students where mark1 <40 or mark2 <40 or mark3 <40`}
+            let marksObj = await sequelize.query(query)
+            res.send((marksObj[0]))
+        }
+    
+        getR().then((o)=>
+        {
+         console.log('done')
+        })
+        .catch((e)=>
+        {
+             console.log('error', e)
+        })
     
   })
 
